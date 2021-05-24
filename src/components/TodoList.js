0ff60418bea08009d5f3de87ad7111102656d6d1
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-05-23 22:34:44 -0400

    delete functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,6 +14,21 @@ function TodoList() {
     console.log(todo, ...todos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+  }
+
+
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id)
+
+    setTodos(removeArr)
+  }
+
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
       if(todo.id === id) {
@@ -27,7 +42,7 @@ function TodoList() {
     <div>
       <h1>What is the Plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
     </div>
   );
 }
