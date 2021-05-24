7d458c9f8699be77732c77ac016af116764043f3
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-05-23 22:25:55 -0400

    add todo functionality

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -10,11 +10,11 @@ function TodoForm(props) {
   const handleSubmit = (e) => {
     e.preventDefault(); //to prevent auto refreshing on button click
 
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input,
-    // });
-    
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
     setInput("")
   };
 
